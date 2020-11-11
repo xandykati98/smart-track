45beaf305e6344d777c4c541caf180160ecfefe5
Alexandre <xandykati98@gmail.com>
2020-11-11 00:54:39 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+## Install
+
+Just download the source or clone this repository 
+
+```
+git clone https://github.com/ailon/minimalist-typescript-webpack-starter.git .
+```
+
+(you may want to delete the `.git` directory to remove the association with this repo)
+
+Then install dependencies:
+```
+npm install
+```
+
+## Run
+
+```
+npm start
+```
+
+## Edit
+
+Edit  `src/index.ts` and add other TypeScript files the way you see fit. Edit `src/assets/template.html` to change the HTML used by webpack.
---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class SmartTrack {
+    disposivo: null | string = null;
+    paginas_visitadas: string[] = [];
+    imoveis_visitados: string[] = [];
+    inicio_sessao: Date = new Date();
+    uuid: string | null = null;
+    referrer: null | string = null;
+
+    constructor() {
+        console.log('SmartTrack iniciado!');
+        const cache_visitante_id = localStorage.getItem('visitante_id')
+        if (cache_visitante_id === null) {
+            const novo_visitante_id = uuidv4();
+            localStorage.setItem('visitante_id', novo_visitante_id);
+            this.uuid = novo_visitante_id;
+        } else {
+            this.uuid = cache_visitante_id;
+        }
+
+        this.referrer = document.referrer;
+        this.disposivo = this.getDispositivo();
+
+        //https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
+        /* These are the modifications: */
+        history.pushState = ( f => function pushState(){
+            var ret = f.apply(this, arguments);
+            window.dispatchEvent(new Event('pushstate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return ret;
+        })(history.pushState);
+
+        history.replaceState = ( f => function replaceState(){
+            var ret = f.apply(this, arguments);
+            window.dispatchEvent(new Event('replacestate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return ret;
+        })(history.replaceState);
+
+        window.addEventListener('popstate',()=>{
+            window.dispatchEvent(new Event('locationchange'))
+        });
+        window.addEventListener('locationchange', () => {
+            console.log('mudança de pagina detectada')
+            this.paginas_visitadas.push(location.pathname)
+            if (location.pathname.indexOf('/imovel/') > -1) {
+                const split_pathname = location.pathname.split('/');
+                this.imoveis_visitados.push(split_pathname[split_pathname.length - 1]);
+            } else if (location.pathname.indexOf('/perfilImovel') > -1) {
+                const search_url = new URL(location.href);
+                var id_imovel = search_url.searchParams.get("id_imovel");
+                this.imoveis_visitados.push(id_imovel);
+            }
+        })
+    }
+    //https://dev.to/itsabdessalam/detect-current-device-type-with-javascript-490j
+    getDispositivo = () => {
+        const ua = navigator.userAgent;
+        if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+          return "tablet";
+        }
+        if (
+          /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+            ua
+          )
+        ) {
+          return "mobile";
+        }
+        return "desktop";
+    };
+}
+
+declare global {
+    interface Window {
+        SmartTrack: typeof SmartTrack;
+    }
+}
+
+
+window.SmartTrack = SmartTrack;
